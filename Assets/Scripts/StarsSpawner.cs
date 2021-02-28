39d625a14e946a704b2cdd636388721f3b39f88d
Aliev Rasul <arasul110294@gmail.com>
2021-03-01 00:31:51 +0300

    fixed trophy animation; added stars spawner;

---
+++ Assets/Scripts/StarsSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsSpawner : MonoBehaviour
+{
+    [SerializeField] private int startToAdd = 2;
+
+    private StarDisplay _starDisplay;
+
+    private void Awake()
+    {
+        _starDisplay = FindObjectOfType<StarDisplay>();
+    }
+
+    public void AddStar()
+    {
+        _starDisplay.AddStars(startToAdd);
+    }
+
+}
