d05bc6b05431177c6d10279ed23905b1b45d688c
Aliev Rasul <arasul110294@gmail.com>
2021-02-28 20:05:32 +0300

    added loading and start scenes;

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private float waitForLoad; 
+    private int _currentSceneIndex;
+
+    private void Start()
+    {
+        _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (_currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadWithDelay());
+        }
+    }
+
+    private IEnumerator LoadWithDelay()
+    {
+        yield return new WaitForSeconds(waitForLoad);
+        LoadNextScene();
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(_currentSceneIndex + 1);
+    }
+}
