ab2d2b1411a350d2ecae2049a0c305a36ca45018
Aliev Rasul <arasul110294@gmail.com>
2021-02-28 23:10:25 +0300

    added defender button;

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    [SerializeField] private Color32 defaultColor;
+    
+    public SpriteRenderer SpriteRenderer => _spriteRenderer;
+    private SpriteRenderer _spriteRenderer;
+
+    private List<DefenderButton> _buttons;
+
+    private void Awake()
+    {
+        _buttons = FindObjectsOfType<DefenderButton>().ToList();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.color = defaultColor;
+    }
+
+    private void OnMouseDown()
+    {
+        foreach (var button in _buttons)
+        {
+            button.SpriteRenderer.color = defaultColor;
+        }
+        
+        _spriteRenderer.color = Color.white;
+    }
+}
