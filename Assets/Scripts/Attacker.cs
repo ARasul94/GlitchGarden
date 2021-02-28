345624e215e65f45dc241aede9cee72bdb9557a3
Aliev Rasul <arasul110294@gmail.com>
2021-02-28 21:22:22 +0300

    added events to animation for controlling speed; added spawner prefab;

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [SerializeField][Range(0,3)] private float moveSpeed;
+
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+    }
+
+    public void SetMoveSpeed(float speed)
+    {
+        moveSpeed = speed;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private float minTimeDelay = 2f;
+    [SerializeField] private float maxTimeDelay = 7f;
+    [SerializeField] private Attacker attacker;
+    [SerializeField] private bool spawn = true;
+
+    private IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minTimeDelay, maxTimeDelay));
+            Instantiate(attacker, transform.position, Quaternion.identity);
+        }
+    }
+}
