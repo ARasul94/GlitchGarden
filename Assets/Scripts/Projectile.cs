695e01b48a42f9b10b430fb3189507433cc7f163
Aliev Rasul <arasul110294@gmail.com>
2021-02-28 22:31:40 +0300

    added health and projectile script;

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private int damage = 50;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var health = other.gameObject.GetComponent<Health>();
+        var attacker = other.gameObject.GetComponent<Attacker>();
+
+        if (health && attacker)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
