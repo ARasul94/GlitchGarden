c76abb6bf2c4820ce1888a4d3a6b8802904280d6
Aliev Rasul <arasul110294@gmail.com>
2021-02-28 21:40:32 +0300

    added cactus prefab; cactus animations;

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private Transform gun;
+    
+    
+    public void Fire()
+    {
+        Instantiate(projectile, gun.position, gun.rotation);
+        return;
+    }
+}
