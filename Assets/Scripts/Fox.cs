91e4e17fd386e7d6d251069e405238477ad0282d
Aliev Rasul <arasul110294@gmail.com>
2021-03-01 19:01:07 +0300

    added fox attacker;

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Attacker), typeof(Animator))]
+public class Fox : MonoBehaviour
+{
+    private Attacker _attacker;
+    private Animator _animator;
+    private readonly int _isJumping = Animator.StringToHash("IsJumping");
+
+    private void Awake()
+    {
+        _attacker = GetComponent<Attacker>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var defender = other.GetComponent<Defender>();
+
+        if (!defender)
+            return;
+
+        var gravestone = other.GetComponent<Gravestone>();
+        if (gravestone)
+        {
+            _animator.SetTrigger(_isJumping);
+        }
+        else
+        {
+            _attacker.Attack(defender);
+        }
+        
+    }
+}
