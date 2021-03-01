2d2a9a64f671e96e3134a1873f0ceac5110c7330
Aliev Rasul <arasul110294@gmail.com>
2021-03-01 21:06:45 +0300

    added level timer;

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Slider))]
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("Level time is seconds")]
+    [SerializeField] private int levelTime = 10;
+    
+    private Slider _slider;
+
+    private void Awake()
+    {
+        _slider = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        var time = Time.timeSinceLevelLoad / levelTime;
+
+        _slider.value = time;
+    }
+}
