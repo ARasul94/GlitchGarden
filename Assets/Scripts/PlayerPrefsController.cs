195b4de70e457e3547d2e9204febd4c032e05d28
Aliev Rasul <arasul110294@gmail.com>
2021-03-05 21:28:46 +0300

    filled option scene;

---
+++ Assets/Scripts/PlayerPrefsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerPrefsController
+{
+    private const string VOLUME_KEY = "volume";
+    private const string DIFFICULTY_KEY = "difficulty";
+
+    private const float MIN_VOLUME = 0;
+    private const float MAX_VOLUME = 1;
+
+    public static void SetVolume(float volume)
+    {
+        if (volume >= MIN_VOLUME && volume <= MAX_VOLUME)
+        {
+            PlayerPrefs.SetFloat(VOLUME_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError($"Not allowed volume value. Try between {MIN_VOLUME} and {MAX_VOLUME}");
+        }
+    }
+
+    public static float GetVolume()
+    {
+        return PlayerPrefs.GetFloat(VOLUME_KEY);
+    }
+}
